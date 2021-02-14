e1b008ec6daf31e5c2f7eeeec87aeda4c7339f80
Ricardo Martins <martinsc.ricardo@gmail.com>
2021-02-14 12:02:57 -0300

    fix: calendar

--- yume/src/app/calendario/calendario.component.ts
+++ yume/src/app/calendario/calendario.component.ts
@@ -10,92 +10,90 @@ export class CalendarioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    const date = new Date();
+  
+    const renderCalendar = () => {
+      date.setDate(1);
+  
+      const monthDays = document.querySelector(".days");
+  
+      const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+      ).getDate();
+  
+      const prevLastDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+      ).getDate();
+  
+      const firstDayIndex = date.getDay();
+  
+      const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+      ).getDay();
+  
+      const nextDays = 7 - lastDayIndex - 1;
+  
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+  
+      let dateHeading1 = document.querySelector(".date h1");
+      if (dateHeading1) dateHeading1.innerHTML = months[date.getMonth()];
+  
+      let dateParagraph = document.querySelector(".date p");
+      if (dateParagraph) dateParagraph.innerHTML = new Date().toDateString();
+  
+      let days = "";
+  
+      for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+      }
+  
+      for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+          days += `<div class="today">${i}</div>`;
+        } else {
+          days += `<div>${i}</div>`;
+        }
+      }
+  
+      for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+      }
+
+      if (monthDays) monthDays.innerHTML = days;
+    };
+  
+    document.querySelector(".prev")?.addEventListener("click", () => {
+      date.setMonth(date.getMonth() - 1);
+      renderCalendar();
+    });
+  
+    document.querySelector(".next")?.addEventListener("click", () => {
+      date.setMonth(date.getMonth() + 1);
+      renderCalendar();
+    });
+  
+    renderCalendar();
   }
-/*
- const date = new Date();
-
-const renderCalendar = () => {
-  this.date.setDate(1);
-
-  const monthDays = document.querySelector(".days");
-
-  const lastDay = new Date(
-    this.date.getFullYear(),
-    this.date.getMonth() + 1,
-    0
-  ).getDate();
-
-  const prevLastDay = new Date(
-    this.date.getFullYear(),
-    this.date.getMonth(),
-    0
-  ).getDate();
-
-  const firstDayIndex = this.date.getDay();
-
-  const lastDayIndex = new Date(
-    this.date.getFullYear(),
-    this.date.getMonth() + 1,
-    0
-  ).getDay();
-
-  const nextDays = 7 - lastDayIndex - 1;
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-document.querySelector(".date h1").innerHTML = months[this.date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-  let days = "";
-
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  }
-
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      this.date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
-    } else {
-      days += `<div>${i}</div>`;
-    }
-  }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
-  }
-};
-
-document.querySelector(".prev").addEventListener("click", () => {
-  Date.setMonth(Date.getMonth() - 1);
-  renderCalendar();
-});
-
-document.querySelector(".next").addEventListener("click", () => {
-  Date.setMonth(Date.getMonth() + 1);
-  renderCalendar();
-});
-
-renderCalendar();
-
-*/
 
 
 }
